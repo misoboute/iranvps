dc5b8ee4bbe2471f12e7f272390884ad717845a0
Muhammad Ismail Soboute <misoboute@gmail.com>
2023-01-21 22:28:51 +0000

    Polish, add comments, test and debug on server

--- script/local-setup.sh
+++ script/local-setup.sh
@@ -3,24 +3,52 @@
 set -ex
 
 script_dir=$(realpath $(dirname $BASH_SOURCE[0]))
-host=$1
-user=${2:-$USER}
+host=$1 # Remote host (server IP address)
+user=${2:-$USER} # New user to create on the server
+install_prefix=${3:-/home/$user/srvsetup} # Path on the server to install the config script and resources
 
+# Enable logging on as root to the host using public key authentication (we'll disable p/w authentication later)
 ssh-copy-id root@$host
 
-ssh root@$host adduser $user --add_extra_groups
-ssh root@$host adduser $user sudo
+# Add the new user (if not there already) and add it to sudo group
+ssh root@$host "
+user_exists=\$(grep ^$user /etc/passwd | cut -d: -f1)
+if [[ -z \"\$user_exists\" ]]; then
+    adduser $user --add_extra_groups;
+    adduser $user sudo;
+fi"
 
+# Enable logging on to the as the new user using public key authentication
 ssh-copy-id $user@$host
 
+# Generate CIDR addresses files from the IP range source files for Iran and Ireland IP ranges
+if [[ ! -f $script_dir/iran-ip-range-cidr.txt ]]
+then
+    for ip_range in $(cat --squeeze-blank ${script_dir}/iran-ip-range.txt)
+    do
+        ipcalc -r $ip_range | tail -n +2 >> iran-ip-range-cidr.txt
+    done    
+fi
+
+if [[ ! -f $script_dir/irish-ip-range-cidr.txt ]]
+then
+    for ip_range in $(cat --squeeze-blank ${script_dir}/irish-ip-range.txt)
+    do
+        ipcalc -r $ip_range | tail -n +2 >> irish-ip-range-cidr.txt
+    done    
+fi
+
+# Copy config script and resources to the server
 ssh $user@$host mkdir -p /home/$user/srvsetup
 files_to_copy=(
     $script_dir/iran-ip-range.txt
+    $script_dir/iran-ip-range-cidr.txt
     $script_dir/irish-ip-range.txt
+    $script_dir/irish-ip-range-cidr.txt
     $script_dir/server-setup.sh
-    $script_dir/squid-allow-irish-ips.conf
-    $script_dir/squid-irish-ips-proxy-auth-access.conf
+    $script_dir/sshd-no-pw.conf
 )
-scp ${files_to_copy[@]} scp://$user@$host//home/$user/srvsetup/
+scp ${files_to_copy[@]} scp://$user@$host/$install_prefix/
 
-ssh $user@$host bash /home/$user/server-setup.sh $user
+# Execute the config script on the server
+ssh $user@$host bash $install_prefix/server-setup.sh $user
--- script/server-setup.sh
+++ script/server-setup.sh
@@ -5,46 +5,54 @@ set -ex
 script_dir=$(realpath $(dirname $BASH_SOURCE[0]))
 htuser=${1:-$USER}
 
-sudo apt update
-sudo apt upgrade
-sudo apt install squid apache2-utils --assume-yes
+# Update sofware and install new ones
+sudo -S apt update
+sudo -S apt upgrade --assume-yes
+sudo -S apt install squid apache2-utils --assume-yes
 
-systemctl status squid.service
+# Stop the proxy service until all configuration is done
+sudo -S systemctl start squid.service
 
-mkdir -p ~/backup
-cp /etc/squid/squid.conf ~/backup/
+# Create the new user using htpasswd if it doesn't exist
+htuser_file=/etc/squid/passwords
+htuser_exists=$(grep "^$htuser" /etc/passwd | cut -d: -f1)
+if [[ -z "$htuser_exists" ]]; then
+    sudo -S htpasswd -c $htuser_file $htuser
+fi
 
-sudo htpasswd -c /etc/squid/passwords $htuser
-
+# Create squid custom config file to allow only Irish IP addresses and only connections authenticated using basic authentication:
 squid_cfg_file=${TMPDIR:-/tmp}/squid-irish-ips-proxy-auth-access.conf
-
-awk '{ print "acl", "irish_ips", "src", $1 }' irish-ip-range.txt > $squid_cfg_file
-cat << EOF
+awk '{ print "acl", "irish_ips", "src", $1 }' $script_dir/irish-ip-range.txt > $squid_cfg_file
+cat >> $squid_cfg_file << EOF
 
 http_access allow irish_ips
 
-auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid/passwords
+auth_param basic program /usr/lib/squid/basic_ncsa_auth $htuser_file
 auth_param basic realm proxy
 
 acl authenticated proxy_auth REQUIRED
 
 http_access allow authenticated
-EOF >> $squid_cfg_file
+EOF
 
-sudo cp $squid_cfg_file /etc/squid/conf.d/
+# Copy the custom config file where it will be picked up and read by squid during configuration
+sudo -S cp $squid_cfg_file /etc/squid/conf.d/
 
-sudo systemctl restart squid.service
+# Disable SSH logon using password -- SSH public keys must be already added to ~/.ssh/authorized_keys e.g. using ssh-copy-id
+sudo -S cp $script_dir/sshd-no-pw.conf /etc/ssh/sshd_config.d/
 
-sudo cp ~/sshd-no-pw-no-root.conf /etc/ssh/sshd_config.d/
-
-for ip_range in $(cat --squeeze-blank ${script_dir}/irish-ip-range.txt ${script_dir}/iran-ip-range.txt)
+# Only allow Irish and Iranian IP addresses to connect to ssh
+for ip_range in $(cat --squeeze-blank ${script_dir}/irish-ip-range-cidr.txt ${script_dir}/iran-ip-range-cidr.txt)
 do
-    sudo ufw allow from ${ip_range} app OpenSSH
+    sudo -S ufw allow from ${ip_range} app OpenSSH
 done
 
-for ip_range in $(cat --squeeze-blank ${script_dir}/irish-ip-range.txt)
+# Only allow Irish IP addresses to connect to Squid
+for ip_range in $(cat --squeeze-blank ${script_dir}/irish-ip-range-cidr.txt)
 do
-    sudo ufw allow from ${ip_range} app Squid
+    sudo -S ufw allow from ${ip_range} app Squid
 done
 
-sudo ufw enable
+# Show time!
+sudo -S ufw enable
+sudo -S systemctl start squid.service
