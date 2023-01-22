444b013e3272a7e8bf63b3e4ef19e2f5da927491
Muhammad Ismail Soboute <misoboute@gmail.com>
2023-01-22 13:16:10 +0000

    Reboot server after config; fix mistake

--- script/server-setup.sh
+++ script/server-setup.sh
@@ -11,7 +11,7 @@ sudo -S apt upgrade --assume-yes
 sudo -S apt install squid apache2-utils --assume-yes
 
 # Stop the proxy service until all configuration is done
-sudo -S systemctl start squid.service
+sudo -S systemctl stop squid.service
 
 # Create the new user using htpasswd if it doesn't exist
 htuser_file=/etc/squid/passwords
@@ -41,6 +41,7 @@ sudo -S cp $squid_cfg_file /etc/squid/conf.d/
 # Disable SSH logon using password -- SSH public keys must be already added to ~/.ssh/authorized_keys e.g. using ssh-copy-id
 sudo -S cp $script_dir/sshd-no-pw.conf /etc/ssh/sshd_config.d/
 
+# TODO: Directly use iptables instead of ufw
 # Only allow Irish and Iranian IP addresses to connect to ssh
 for ip_range in $(cat --squeeze-blank ${script_dir}/irish-ip-range-cidr.txt ${script_dir}/iran-ip-range-cidr.txt)
 do
@@ -56,3 +57,4 @@ done
 # Show time!
 sudo -S ufw enable
 sudo -S systemctl start squid.service
+sudo reboot
